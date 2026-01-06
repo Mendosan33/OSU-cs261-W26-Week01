97829ac6781cf8eee8bc9db478ed539e9fff7bbb
Mendosan33 <mendosan@oregonstate.edu>
2026-01-06 13:56:49 -0800

    Create hello_gradescope.py

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Santiago Mendoza
+# OSU Email: mendosan@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: hello_gradescope
+# Due Date: 1/14/26
+# Description: Checks to confirm the connection with Gradescope
+
+
+def gradescope_test():
+    return 42
